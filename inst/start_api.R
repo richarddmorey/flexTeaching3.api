64d911f7f43e5c955432905ecf01ab75290f9caa
Richard Morey <richarddmorey@gmail.com>
2023-05-20 13:34:03 +0100

    use root lgr logger in example R file

--- inst/start_api.R
+++ inst/start_api.R
@@ -1,5 +1,6 @@
 
 library(flexTeaching3.api)
+library(lgr)
 
 tmpdir = '/tmp/ft3_scratch'
 if(dir.exists(tmpdir)){
@@ -13,13 +14,12 @@ flexTeaching3.api::ft3_options(cache_location = tmpdir)
 flexTeaching3.api::ft3_options(scratch_dir = tmpdir)
 flexTeaching3.api::ft3_options(errors_to_client = TRUE)
 
-# Start logger and write to file
-lg_api = lgr::get_logger('flexTeaching3.api')
-lg_api$set_threshold('debug')
+# Use root logger lgr and write to file
+lgr$set_threshold('debug')
 format(Sys.time(), "ft3_api_log_%Y-%m-%d_%H:%M.json.log") |>
   file.path(tmpdir,x=_) |>
   lgr::AppenderJson$new() |>
-  lg_api$add_appender(name = 'json')
+  lgr$add_appender(name = 'json')
 
 flexTeaching3.api::ft3_serve_api()
 
