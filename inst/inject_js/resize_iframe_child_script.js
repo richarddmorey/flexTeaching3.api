d18c44998f43a4f3888cab38bcbf60b36ba53c88
Richard Morey <richarddmorey@gmail.com>
2023-06-15 07:35:50 +0100

    change iframe resizer to 200ms interval

--- inst/inject_js/resize_iframe_child_script.js
+++ inst/inject_js/resize_iframe_child_script.js
@@ -17,10 +17,13 @@ window.addEventListener("DOMContentLoaded", (event) => {
 
   // Do this 5 times, once every 500ms, just to make sure we 
   // account for slow scripts (eg MathJax)
-  for (let i = 0; i < 5; i++) {
-    setTimeout( sendHeight, 500 * i);
-  }
-  //setInterval(sendHeight, 500);
+  // for (let i = 0; i < 5; i++) {
+  //  setTimeout( sendHeight, 500 * i);
+  // }
+  
+  // Some widgets change size later, eg, surveys, so poll every
+  // 200ms
+  setInterval(sendHeight, 200);
 });
 
 //]]>
